814ee0bc7d9aac66b11231b586b7b2d12b76fcf6
bobzz-zone <bobzz.zone@gmail.com>
2016-12-22 11:40:32 +0700

    www

--- gias/additional_tools/doctype/combine_deliveries/test_combine_deliveries.py
+++ gias/additional_tools/doctype/combine_deliveries/test_combine_deliveries.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015, Bobby and Contributors
+# Copyright (c) 2015, Ros@gmail.com and Contributors
 # See license.txt
 from __future__ import unicode_literals
 
--- gias/additional_tools/doctype/tt_document/tt_document.js
+++ gias/additional_tools/doctype/tt_document/tt_document.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Bobby and contributors
+// Copyright (c) 2016, Ros@gmail.com and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('TT Document', {
